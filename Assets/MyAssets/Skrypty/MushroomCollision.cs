d6f8425e9c563cae8d117037331cc85ce899e828
Ola <alekchr042@gmail.com>
2020-04-08 18:38:33 +0200

    Odbijanie od drzew

--- Assets/MyAssets/Skrypty/MushroomCollision.cs
+++ Assets/MyAssets/Skrypty/MushroomCollision.cs
@@ -28,9 +28,4 @@ public class MushroomCollision : MonoBehaviour
             Destroy(gameObject);
         }
     }
-
-
-
-
-
 }
---
+++ Assets/MyAssets/Skrypty/TreeCollision.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        var isCollisionWithPlayer = other.gameObject.tag == "Player";
+        if (isCollisionWithPlayer)
+        {
+            Debug.Log("collision with player");
+            var rb = other.gameObject.GetComponent<Rigidbody>() ;
+            rb.AddForce(rb.gameObject.transform.forward * -50.0f, ForceMode.Impulse);
+            
+        }
+    }
+
+    //void OnCollisionEnter(Collision collision)
+    //{
+    //    var isCollisionWithPlayer = collision.gameObject.tag == "Player";
+    //    if (isCollisionWithPlayer)
+    //    {
+    //        Debug.Log("collision with player");
+    //        collision.rigidbody.AddForce(collision.gameObject.transform.forward * -2, ForceMode.Impulse);
+    //    }
+    //}
+}
